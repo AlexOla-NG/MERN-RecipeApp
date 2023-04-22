0bb0e3330304daba5e384d61b0fd8f5213e01030
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-04-22 13:52:27 +0100

    🔥feat: setup routes, scss folder & files

--- mern-recipe-app/src/main.tsx
+++ mern-recipe-app/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./react-query";
+import App from "./App";
+import "./css/main.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</QueryClientProvider>
+	</React.StrictMode>
+);
